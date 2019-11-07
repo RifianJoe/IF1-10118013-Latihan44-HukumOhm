d9241b14cce0b089c1da5be9fa481e51701f214c
Rifian Joe Premilenio <55788355+RifianJoe@users.noreply.github.com>
2019-11-07 21:33:05 +0800

    Add files via upload

---
+++ IF1-10118013-Latihan44-HukumOhm/src/if1/pkg10118013/latihan44/hukumohm/IF110118013Latihan44HukumOhm.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118013.latihan44.hukumohm;
+
+/**
+ *
+ * @author Rifian
+ * Nama : Rifian Joe Premilenio
+ * Kelas : IF-01
+ * NIM : 10118013
+ */
+public class IF110118013Latihan44HukumOhm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Baterai bateray = new  Baterai();
+        
+        Baterai baterayy = new Baterai(3, 12);
+        
+    }
+    
+}
